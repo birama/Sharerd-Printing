7d5b8c259b99ab3582bcf9a67bf6504168049a09
Sunny S <3030712@gmail.com>
2013-06-17 15:02:48 -0700

    Protocol Interface takes string as url to connect as/to.

--- general/protocol/protocolinterface.cs
+++ general/protocol/protocolinterface.cs
@@ -1,9 +1,13 @@
 namespace general{
 
-public interface IProtocol{
-	public bool connect(){}
-	public void dissconnect(){}
-	public bool send(){}
-	public bool recv(){}
+public interface IProtocol<Ttransertype>{
+
+/* Connect
+   + Takes T and string as url.
+*/
+public bool connect(Ttranfertype, string);
+public void dissconnect();
+public bool send();
+public bool recv();
 }
 }
