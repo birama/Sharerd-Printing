91d9862e233a002d1f81c58f2c8470eeb16e13a2
V <james187t75@gmail.com>
2013-06-17 13:53:09 -0700

    Files added

---
+++ client/client.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting;
+using general;
+using System;
+using System.Runtime.Remoting.Channels.Http;
+using System.Runtime.Remoting.Channels;
+
+namespace client
+{
+    class client
+    {
+        delegate void SetValueDelegate(int value);
+        delegate String GetNameDelegate();
+
+        static void Main(string[] args)
+        {
+            HttpChannel channel = new HttpChannel();
+            ChannelServices.RegisterChannel(channel);
+            IPrintTask obj = (IPrintTask)Activator.GetObject(
+            typeof(IPrintTask),
+            "http://localhost:1234/MyRemoteObject.soap");
+            Console.WriteLine("Client.Main(): Reference to rem.obj. acquired");
+            //Console.WriteLine(obj.GetPrintName());
+            //SetValueDelegate svDelegate = new SetValueDelegate(obj.SetPrintTask(v1, v2, v3));
+            SetValueDelegate svDelegate = new SetValueDelegate(obj.SetCopies);
+            IAsyncResult svAsyncres = svDelegate.BeginInvoke(1, null, null);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ general/general.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace general
+{
+    public interface IPrintTask
+    {
+        //void SetValue(int newval);
+        //int GetValue();
+        void SetPrintTask(string userName, string printName, int printCopies);
+        String GetUserName();
+        void SetUserName(string name);
+        String GetPrintName();
+        void SetPrintName(string name);
+        int GetCopies();
+        void SetCopies(int copies);
+
+    }
+
+    public interface IPrinter
+    {
+        //void SetValue(int newval);
+        //int GetValue();
+        // String GetName();
+    }
+}
