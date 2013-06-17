38c0a1e25d6e77f5d2465b5fe73cbc0a5ed804a3
Sunny S <3030712@gmail.com>
2013-06-17 16:23:54 -0700

    Seperated Printer and PrintTask objects from server code, and placed them inside lib.

---
+++ general/Printer.cs
@@ -0,0 +1,83 @@
+namespace general{
+class Printer : MarshalByRefObject, IPrinter{
+        string printerName;
+        Queue<string> printerTasks = new Queue<string>();
+        //bool active;
+        bool idle;
+
+        public Printer()
+        {
+            Console.WriteLine("Printer(): \t\t-New Empty Printer created");
+            idle = CheckIdle();
+            
+        }
+
+        public Printer(string printerName)
+        {
+            SetPrinterName(printerName);
+            Console.WriteLine("Printer(): \t\t-New Full Printer created : " + GetPrinterName() );
+            idle = CheckIdle();
+        }
+
+        //GETTERS AND SETTERS
+
+        public string GetPrinterName()
+        {
+            return printerName;
+        }
+
+        public void SetPrinterName(string name)
+        {
+            printerName = name;
+        }
+
+
+        public bool CheckIdle()
+        {
+            if (this.printerTasks.Count == 0)
+            {
+                Console.WriteLine("CheckIdle(): \t\t-Set to Idle");
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine("CheckIdle(): \t\t-Set to Busy");
+                return false;
+            }
+        }
+
+        public void LoadPrintTask(string printName)
+        {
+
+            printerTasks.Enqueue(printName);
+
+            if (idle == false)
+            {
+                Console.WriteLine("LoadPrintTask(): \t-Busy");
+            }
+
+            else
+            {
+                Console.WriteLine("LoadPrintTask(): \t-Idle");
+                Print(); 
+            }
+        }
+
+        public void Print()
+        {
+            Thread.Sleep(1300);
+            Console.WriteLine(printerTasks.Dequeue() + "PRINTED");
+
+            if (!CheckIdle())
+            {
+                Print();
+            }
+
+            else
+            {
+                Console.WriteLine ("Print() \t\t-All Tasks Completed");
+            }
+        }
+    }
+}
