8de38a9b57924bc1bfdc9bfd05f8092f914efa37
Sunny S <3030712@gmail.com>
2013-06-17 21:39:02 -0700

    Added main.cs as the main start point of execution.

---
+++ main.cs
@@ -0,0 +1,18 @@
+﻿using general;
+using general.Server;
+using general.Client;
+
+namespace Shared-Printing{
+
+/* Sharerd Printing main
+	+ used to start either a client or server using client or server classes.
+	+ seperates the configuration from the startup process.
+*/
+class Shared-Printing{
+
+static void Main(string[] args){
+
+}
+
+}
+}
