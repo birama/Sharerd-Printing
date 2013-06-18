ea581e86be92ab57bccb1d17868d41c580b25c32
Sunny S <3030712@gmail.com>
2013-06-17 17:46:51 -0700

    Made computer interface take template printer & user types.

--- general/computerinterface.cs
+++ general/computerinterface.cs
@@ -1,6 +1,8 @@
 namespace general{
 
-public interface IComputer{
+public interface IComputer<Tprinter, Tuser>{
+	public Tprinter[] printer = new Tprinter[];
+	public Tuser[] user = new Tuser[];
 	public string name;
 }
 
