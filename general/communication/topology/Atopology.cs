e574a92f273c2181b056ff61f939f00130d2a6c1
nhiemtruong <nhiemtruongm@gmail.com>
2013-06-18 11:51:02 -0600

    Fixed spelling errors

--- general/communication/topology/Atopology.cs
+++ general/communication/topology/Atopology.cs
@@ -2,21 +2,21 @@ using general;
 
 namespace general{
 
-public abstract class ATopology<Ttransertype>{
+public abstract class ATopology<Ttransfertype>{
 //private
 //private IUser[] user = new IUser[2];
 
 //protected
-protected IProtocol<Ttransertype> protocol;
+protected IProtocol<Ttransfertype> protocol;
 
 //public
 public string name = "Default Abstract Topology!";
 
-public ATopology(IProtocol<Ttransertype> protocol){
+public ATopology(IProtocol<Ttransfertype> protocol){
 this.protocol = protocol;
 }
 
-abstract public bool connect(Ttransertype obj);
+abstract public bool connect(Ttransfertype obj);
 abstract public void disconnect();
 
 }
