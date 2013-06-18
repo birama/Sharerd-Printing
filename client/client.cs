72e9d9d75a9c6ddb91241637cc7a786bcf3cc081
Nhiem Truong <nhiemtruongm@gmail.com>
2013-06-17 17:29:16 -0700

    client update

--- client/client.cs
+++ client/client.cs
@@ -26,6 +26,33 @@ namespace client
             //SetValueDelegate svDelegate = new SetValueDelegate(obj.SetPrintTask(v1, v2, v3));
             SetValueDelegate svDelegate = new SetValueDelegate(obj.SetCopies);
             IAsyncResult svAsyncres = svDelegate.BeginInvoke(1, null, null);
+			
+			Console.WriteLine("Client.Main(): Will call getPrintName()");
+			GetPrintNameDelegate gnPrintDelegate = new GetPrintNameDelegate(obj.GetPrintName);
+			IAsyncResult gnAsyncres = gnPrintDelegate.BeginInvoke(null,null);
+		
+			
+			Console.WriteLine("Client.Main(): Will call getUserName()");
+			GetUserNameDelegate gnUserDelegate = new GetUserNameDelegate(obj.GetUserName);
+			IAsyncResult gnAsyncres2 = gnUserDelegate.BeginInvoke(null,null);
+			
+
+			Console.WriteLine("Client.Main(): EndInvoke for setValue()");
+			svDelegate.EndInvoke(svAsyncres);
+			
+			Console.WriteLine("Client.Main(): EndInvoke for getPrintName()");
+			String printname = gnPrintDelegate.EndInvoke(gnAsyncres);
+			
+			Console.WriteLine("Client.Main(): EndInvoke for getUserName()");
+			String username = gnUserDelegate.EndInvoke(gnAsyncres2);
+
+			Console.WriteLine("Client.Main(): received name {0}",printname);
+			Console.WriteLine("Client.Main(): received name {0}",username);
+
+			Console.WriteLine("Client.Main(): Will now read value");
+			int copy = obj.GetCopies();
+			Console.WriteLine("Client.Main(): New server side value {0}", copy);
+			
             Console.ReadLine();
 
         }
