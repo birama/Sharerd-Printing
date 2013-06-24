9b05446c44c9cb08a161305d9d729ee329646e70
Sunny S <3030712@gmail.com>
2013-06-24 02:25:32 -0700

    Client:IApplication added with basic member code.

---
+++ client/Client.cs
@@ -0,0 +1,33 @@
+using general;
+using log4net;
+using log4net.Config;
+
+namespace Client{
+public class Client : IApplication{
+
+	private ATopology<IPrintTask> comm;
+	readonly private ILog log = LogManger.GetLogger (typeof(Client));
+
+	public Client() {
+		BasicConfigurator.Configure();
+	}
+
+	public bool init() {
+		log.Info ("Initlzing Client.");
+		return true;
+	}
+
+	public bool start() {
+		log.Info ("Client Started.");
+		this.comm.connect (ATopology<IPrintTask>.role.client);
+		return true;
+	}
+
+	public bool stop() {
+		log.Info("Stopping Client");
+		this.comm.disconnect();
+		return true;
+	}
+
+}
+}
