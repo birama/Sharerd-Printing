00fcda59dee01d9f745dbb9a2e6f1e798c0fd6d3
Sunny S <3030712@gmail.com>
2013-06-23 18:02:13 -0700

    Man handles null and 0 args & init and starts app obj.

--- main.cs
+++ main.cs
@@ -1,22 +1,31 @@
 ﻿using general;
 using Server;
-using Client;
 
-namespace SharedPrinting{
-
+//using Client;
+namespace SharedPrinting {
 /* Sharerd Printing main
 	+ used to start either a client or server using client or server classes.
 	+ seperates the configuration from the startup process.
 */
-class SharedPrinting{
-static void Main(string[] args){
-IApplication app;
-	if (args[0] == "server"){
-	app = new Server.Server();
-	} else if ((args[0]) == "client"){
-	app = new Client.Client();
-	}
-}
+class SharedPrinting {
+	static void Main (string[] args) {
+		IApplication app = null;
+		if (args == null || args.Length < 1) {
+			app = new Server.Server ();
+		} else {
+			if (args [0] == "server") {
+				app = new Server.Server ();
+			} else if (args [0] == "client") {
+//	app = new Client.Client();
+				} else {
 
+				}
+		}
+		if (app != null) {
+			if (app.init ()) {
+				app.start ();
+			}
+		}
+	}
 }
 }
