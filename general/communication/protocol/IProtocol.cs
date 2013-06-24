d0c1954606b2efb9c0ffa8c2eda3977f09212b46
Sunny S <3030712@gmail.com>
2013-06-24 01:57:05 -0700

    Added send, listen and recv members to saop, and log4net logging.

--- general/communication/protocol/IProtocol.cs
+++ general/communication/protocol/IProtocol.cs
@@ -4,12 +4,14 @@ public interface IProtocol<Ttransfertype> {
 /* Connect
    + Takes T and string as url.
 */
-	bool connect (Ttransfertype obj, string url);
+	bool connect (string url, int port);
+
+	bool listen (string url, int port);
 
 	void disconnect ();
 
 	bool send (Ttransfertype obj);
-//Ttransertype recv();
 
+	Ttransfertype recv ();
 }
 }
--- general/communication/protocol/ProtocolSoap.cs
+++ general/communication/protocol/ProtocolSoap.cs
@@ -2,19 +2,35 @@ using System.Runtime.Remoting.Channels.Http;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Messaging;
 using System.Runtime.Remoting;
+using System;
 using general;
+using log4net;
+using log4net.Config;
 
 namespace general {
 public class ProtocolSoap<Ttransfertype> : IProtocol<Ttransfertype> {
 //Private
 	private HttpChannel chnl;
-	private Ttransfertype placeholder;
+	private Ttransfertype remoteobj;
+		readonly private ILog log = LogManager.GetLogger (typeof(IProtocol<Ttransfertype>));
 //Public
-	public ProtocolSoap(int port) {
-		this.chnl = new HttpChannel (port);
+	public ProtocolSoap() {
+		BasicConfigurator.Configure ();
 	}
 
-	public bool connect (Ttransfertype objtype, string url) {
+	public bool connect (string url, int port) {
+		string url2 = "http://" + url + ":" + port.ToString ();
+		log.Info ("Connecting to: " + url2);
+		this.chnl = new HttpChannel ();
+		ChannelServices.RegisterChannel (this.chnl, false);
+		log.Debug ("Getting remote object.");
+		this.remoteobj = (Ttransfertype)Activator.GetObject (typeof(Ttransfertype), url2);
+		return true;
+	}
+
+	public bool listen (string url, int port) {
+		log.Info ("Listing on: " + url + " on port: " + port.ToString());
+		this.chnl = new HttpChannel (port);
 		ChannelServices.RegisterChannel (this.chnl, false);
 		RemotingConfiguration.RegisterWellKnownServiceType (typeof(Ttransfertype), url, WellKnownObjectMode.Singleton);
 		chnl.StartListening (null);
@@ -22,13 +38,18 @@ public class ProtocolSoap<Ttransfertype> : IProtocol<Ttransfertype> {
 	}
 
 	public void disconnect () {
+		log.Info ("Stopping to listen on channel.");
 		this.chnl.StopListening (null);
 	}
 
 	public bool send (Ttransfertype obj) {
-		return true;
+		log.Error ("This protocol does not facilate sending objects. Only changing referaced objects.");
+		return false;
 	}
-//public Ttransfertype recv(return this.placeholder;);
 
+	public Ttransfertype recv () {
+		log.Debug ("Returning remote object to caller.");
+		return this.remoteobj;
+	}
 }
 }
