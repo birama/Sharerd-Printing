7f466d83ec77662b1bd468950a18b1e7d383c5b8
nhiemtruong <nhiemtruongm@gmail.com>
2013-06-25 01:55:35 -0600

    Update Client.Designer.cs

--- client/Client.Designer.cs
+++ client/Client.Designer.cs
@@ -28,11 +28,11 @@ namespace Client
         /// </summary>
         private void InitializeComponent()
         {
-       
             this.btnBrowse = new System.Windows.Forms.Button();
             this.txtFileName = new System.Windows.Forms.TextBox();
             this.btnPrint = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // btnBrowse
@@ -64,11 +64,20 @@ namespace Client
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(27, 103);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(228, 151);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listView);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.txtFileName);
             this.Controls.Add(this.btnBrowse);
@@ -86,5 +95,6 @@ namespace Client
         private System.Windows.Forms.TextBox txtFileName;
         private System.Windows.Forms.Button btnPrint;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView;
     }
 }
