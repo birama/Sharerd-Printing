1a387b8ce86e7cc500f532ea19288175dc2bb40c
nhiemtruong <nhiemtruongm@gmail.com>
2013-06-25 12:17:51 -0600

    Update Client.Designer.cs

--- client/Client.Designer.cs
+++ client/Client.Designer.cs
@@ -32,12 +32,14 @@ namespace Client
             this.txtFileName = new System.Windows.Forms.TextBox();
             this.btnPrint = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.listView = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(165, 29);
+            this.btnBrowse.Location = new System.Drawing.Point(180, 63);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(75, 23);
             this.btnBrowse.TabIndex = 0;
@@ -46,14 +48,14 @@ namespace Client
             // 
             // txtFileName
             // 
-            this.txtFileName.Location = new System.Drawing.Point(27, 31);
+            this.txtFileName.Location = new System.Drawing.Point(27, 63);
             this.txtFileName.Name = "txtFileName";
             this.txtFileName.Size = new System.Drawing.Size(132, 20);
             this.txtFileName.TabIndex = 1;
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(84, 74);
+            this.btnPrint.Location = new System.Drawing.Point(102, 104);
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(75, 23);
             this.btnPrint.TabIndex = 2;
@@ -64,20 +66,39 @@ namespace Client
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // listView
+            // comboBox1
             // 
-            this.listView.Location = new System.Drawing.Point(27, 103);
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(228, 151);
-            this.listView.TabIndex = 3;
-            this.listView.UseCompatibleStateImageBehavior = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(27, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select a Printer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 159);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 68);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.listView);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnPrint);
             this.Controls.Add(this.txtFileName);
             this.Controls.Add(this.btnBrowse);
@@ -95,6 +116,8 @@ namespace Client
         private System.Windows.Forms.TextBox txtFileName;
         private System.Windows.Forms.Button btnPrint;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.ListView listView;
+  
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
