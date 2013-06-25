a95b3c80321b3c8c57c3d363c2745e89e255fa35
nhiemtruong <nhiemtruongm@gmail.com>
2013-06-25 00:30:07 -0700

    Update client.cs

--- client/client.cs
+++ client/client.cs
@@ -20,7 +20,7 @@ namespace Client
     {
         public Form1()
         {
-            // InitializeComponent();
+            InitializeComponent();
         }
        
         public void btnBrowse_Click(object sender, EventArgs e)
