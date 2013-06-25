350d209f7c0ecdbaaf91ae77abb8eb193acdd4cf
nhiemtruong <nhiemtruongm@gmail.com>
2013-06-25 12:19:06 -0600

    Update clientGUI.cs

--- client/clientGUI.cs
+++ client/clientGUI.cs
@@ -1,152 +1,113 @@
-using System;
-using System.Reflection;
-using System.Threading;
-using System.Windows.Forms;
 using general;
-using server;
+using log4net;
 
+using log4net.Config;
+using System;
 
-namespace ClientGUI
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
 {
-    public partial class FrmClient : Form
+
+    public partial class Form1 : Form
     {
-        
-        public FrmClient()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void BtnConnect_Click(object sender, EventArgs e)
+        private ComboBox comboBox1 = new ComboBox();
+        //private ComboBox comboBox3 = new ComboBox();
+         private PrintDocument pdoc = new PrintDocument();
+        // private Label label2 = new Label();
+
+
+
+        private void ClientGUI_Load(object sender, System.EventArgs e)
         {
-            if (terminal != null)
+            //    PrintDocument pdoc = new PrintDocument();
+            string strDefaultPrinter = pdoc.PrinterSettings.PrinterName;
+            foreach (String strPrinter in PrinterSettings.InstalledPrinters)
             {
-                MessageBox.Show("Client is already connected!");
-                return;
-            }
-
-
-
-            try
-            {
-                remoteProxy = m_Terminal.Connect<IRemotingDistributableObject>(
-                    "localhost", 1234, Constants.remoteableObjectUri, Constants.RemotingChannelName);
-
-                try
+                comboBox1.Items.Add(strPrinter);
+                if (strPrinter == strDefaultPrinter)
                 {
-                    remoteProxy.TestConnection();
+                    comboBox1.SelectedIndex = comboBox1.Items.IndexOf(strPrinter);
                 }
-                catch
-                {
-                    terminal.Disconnect();
+            }
+        }
 
-                    throw;
+        public void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //   DialogResult result = openFileDialog1.ShowDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                if (filename.EndsWith(".txt") || filename.EndsWith(".pdf") || filename.EndsWith(".docx") || filename.EndsWith(".doc") || filename.EndsWith(".png") || filename.EndsWith(".jpg"))
+                {
+
+                    textBox1.Text = DateTime.Now.ToString();
+                    Console.WriteLine("File Name:" + filename);
+                    textBox1.Text = openFileDialog1.FileName;
+
                 }
-
-                
-
-                ShowMessage("Connection Established!");
-            }
-            catch (TargetInvocationException ex)
-            {
-                MessageBox.Show("Connection cannot be established!\n" + ex.InnerException);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Connection cannot be established!\n" + ex);
-            }
-        }
-
-        private void MessageRecivedHandler(string message)
-        {
-            ShowMessage(message);
-        }
-
-        private void btnSend_Click(object sender, EventArgs e)
-        {
-            if (terminal == null)
-            {
-                MessageBox.Show("You must connect first!");
-                return;
-            }
-
-            string mes = textBox1.Text;
-
-            try
-            {
-                Thread[] threads = new Thread[1];
-                for (int i = 0; i < 1; i++)
+                else
                 {
-                    threads[i] = new Thread(sendMessageCallback);
+                    // error message
+                    textBox1.Text = "This file is not supported";
                 }
-
-                for (int i = 0; i < 1; i++)
-                {
-                    threads[i].Start(mes);
-                }
-
-              
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Cannot be sent!\n" + ex);
-            }
+            txtFileName.Text = openFileDialog1.FileName;
         }
 
-        private void sendMessageCallback(object obj)
+
+        public class Client : IApplication
         {
-            remoteProxy.SendMessage((string)obj);
-        }
 
-        private void btnDisconnect_Click(object sender, EventArgs e)
-        {
-            Disconnect();
-            ShowMessage("Disconnected");
-        }
+            private ATopology<Computer> comm;
+            readonly private ILog log = LogManager.GetLogger(typeof(Client));
 
-        private void FrmClient_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Disconnect();
-
-        }
-
-        private void Disconnect()
-        {
-            if (terminal == null)
+            public Client()
             {
-                return;
+                BasicConfigurator.Configure();
+                this.comm = new ServerClient<Computer>();
             }
 
-            try
+            public bool init()
             {
-            
-
-                terminal.Disconnect();
-
-                m_Terminal = null;
-            }
-            catch (Exception ex)
-            {
-                ShowMessage("Disconnection Failed!\n" + ex);
+                log.Info("Initlzing Client.");
+                return true;
             }
 
+            private void btnPrint_Click(object sender, EventArgs e)
+            {
+                start();
+            }
+
+            public bool start()
+            {
+
+                log.Info("Client Started.");
+                this.comm.connect(ATopology<Computer>.role.client);
+                this.comm.things[0].setFile("FileName");
+                return true;
+            }
+
+            public bool stop()
+            {
+                log.Info("Stopping Client");
+                this.comm.disconnect();
+                return true;
+            }
         }
 
-
-        private void ShowMessage(string message)
-        {
-            if (InvokeRequired)
-            {
-               
-                BeginInvoke((ThreadStart)delegate
-                {
-                    ShowMessage(message);
-                });
-
-                return;
-            }
-
-            listBox1.Items.Add(message);
-            Refresh();
-        }
     }
 }
+
